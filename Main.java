6f74d7502add620534cf52bc803514cfd03a9890
Ryan Hanlon <rjhanlon@quinnipiac.edu>
2019-11-06 11:27:52 -0500

    default to deal sea

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         API api = new API();
         Scanner scr = new Scanner(System.in);
-        System.out.println("Enter desired URL:");
-        String url = scr.nextLine();
+        // System.out.println("Enter desired URL:");
+        // String url = scr.nextLine();
         System.out.println("Enter search key:");
         String key = scr.nextLine();
         // https://dealsea.com/search?q=router&search_mode=Deals
-        api.queryAPI(url, key);
+        api.queryAPI("https://dealsea.com", key);
     }
 }
