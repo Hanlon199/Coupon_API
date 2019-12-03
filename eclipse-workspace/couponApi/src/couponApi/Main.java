ed0bfe764b4cdf7c0eb3ae563608ffca2b33c086
goat <giovanni.greco@quinnipiac.edu>
2019-12-03 15:18:03 -0500

    instantiated main, fixes errors

--- eclipse-workspace/couponApi/src/couponApi/Main.java
+++ eclipse-workspace/couponApi/src/couponApi/Main.java
@@ -13,7 +13,7 @@ public class Main extends JFrame{
 
 	
     public static void main(String[] args) throws Exception {
-    	
+    		Main main = new Main();
     
      
         API api = new API();
